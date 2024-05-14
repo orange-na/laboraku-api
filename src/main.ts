10767966ced91b61460666d04411091c839d42ab
orange-na <otuy1125abman@gmail.com>
2024-05-14 23:18:17 +0900

    setup db

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT, () => {
+    console.log(`now connecting on port ${process.env.PORT}...`);
+  });
 }
 bootstrap();
