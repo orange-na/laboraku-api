0203840e7af41821b9aaf02312d309bec28f3690
orange-na <otuy1125abman@gmail.com>
2024-05-26 15:43:05 +0900

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [AppService],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getMe() {
+    return await this.prisma.user.findMany();
+  }
+}
